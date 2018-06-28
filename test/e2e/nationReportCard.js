1dab62c32bbda036d32856325186907a80a0f207
Ryan Budhu <ryan.j.budhu@gmail.com>
2018-06-28 16:12:46 -0400

    updated page code

--- test/e2e/nationReportCard.js
+++ test/e2e/nationReportCard.js
@@ -3,24 +3,22 @@ var config = require('../../nightwatch.conf.js');
 module.exports = { // addapted from: https://git.io/vodU0
   '@tags': ['nationReportCard'],
   'Nation Report Card': (client) => {//function(browser) {
-    const skipStep = "//*[contains(text(), 'SKIP\ THIS\ STEP')]"
+    //const skipStep = "//*[contains(text(), 'SKIP\ THIS\ STEP')]"
     const page = client.page.findButton();//browser
 
     page.navigate()   // Navigate to landing page and check the title of the page.
         .waitForElementVisible('body')
-        .assert.title('NDE Core Web');
+        .assert.title('NDE Core Web')
 
               // How to save a screenshot:
               //.saveScreenshot(config.imgpath(browser) + 'nightwatch-roolz.png')
     
-    client    // Skip entering criteria and go to xplore page with no filters.
-        .useXpath()
-        .click(skipStep)
-        .useCss()
+        // Skip entering criteria and go to xplore page with no filters.
+        .click('@skipStep')
         .waitForElementVisible('.modal-dialog')
         .assert.urlEquals("https://www.nationsreportcard.gov/ndecore/xplore/NDE")
-        .click('.submit-button') //Click accept terms button
-        .pause(3000);
+        .click('.submit-button'); //Click accept terms button
+    client.pause(3000);
 
     client.end();
   }
--- test/pages/findButton.js
+++ test/pages/findButton.js
@@ -3,10 +3,25 @@ module.exports = {
         return this.api.launch_url;
     },
     elements: {
-        skipStep: "//*[contains(text(), 'SKIP\ THIS\ STEP')]",
-        mainButton: "//*[contains(text(), 'ENTER\ THE\ MAIN\ NAEP\ DATA\ EXPLORER')]",
-        subSelect: "//*[contains(text(), 'Select\ a\ Subject')]",
-        graSelect: "//*[contains(text(), 'Select\ a\ Grade')]",
-        jurSelect: "//*[contains(text(), 'Select\ a\ Juristiction')]"
+            skipStep: {
+                selector: "//*[contains(text(), 'SKIP\ THIS\ STEP')]",
+                locateStrategy: 'xpath'
+        },
+            mainButton: {
+                selector: "//*[contains(text(), 'ENTER\ THE\ MAIN\ NAEP\ DATA\ EXPLORER')]",
+                locateStrategy: 'xpath'
+        },
+            subSelect: {
+                selector: "//*[contains(text(), 'Select\ a\ Subject')]",
+                locateStrategy: 'xpath'
+        },
+            graSelect: {
+                selector: "//*[contains(text(), 'Select\ a\ Grade')]",
+                locateStrategy: 'xpath'
+        },
+            jurSelect: {
+                selector: "//*[contains(text(), 'Select\ a\ Juristiction')]",
+                locateStrategy: 'xpath'
+        }
     }
 }
